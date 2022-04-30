953f6ee434256da286cfe739008ae665650d662d
Mohsen Kamrani <m.kamrani@hotmail.com>
2022-04-30 17:06:30 +1000

    Add version

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -2,11 +2,13 @@
 
 const { Command } = require('commander');
 const program = new Command();
+const packageJson = require('../package.json')
 
 const create = require('../lib/create');
 
 
 program
+  .version(packageJson.version, '-v, --version')
   .command('create')
   .description('Create new project')
   .alias('c')
