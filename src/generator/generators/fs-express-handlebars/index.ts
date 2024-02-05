63e900cde6741983a0ba1935f97d91705fd8fa84
Mohsen Kamrani <m.kamrani@hotmail.com>
2024-02-05 19:00:35 +1100

    add prettier

--- src/generator/generators/fs-express-handlebars/index.ts
+++ src/generator/generators/fs-express-handlebars/index.ts
@@ -3,6 +3,14 @@ import { ProjectConfig } from "../../user-inputs/prompt-answers";
 
 export default class FullStackExpressHandlebarsGenerator implements Generator {
   async generate(projectConfig: ProjectConfig): Promise<void> {
+    // copy the scaffold to the output directory
+
+
+    // render the handlebars template
+
+
+    // clean up the project
+
      await Promise.all([]);
      return
   }
--- src/generator/user-inputs/prompt-answers.ts
+++ src/generator/user-inputs/prompt-answers.ts
@@ -11,4 +11,13 @@ type SplitStackProjectConfig = {
   feAnswers: Answers
 }
 
-export type ProjectConfig = (FullStackProjectConfig | SplitStackProjectConfig ) & { base: Answers}
+interface BaseConfig extends Answers {
+  projectName: string
+dataDefinitionsPath: string
+fullStack: string
+selectedBackEnd: string
+selectedFrontEnd: string
+selectedFullStack: string
+}
+
+export type ProjectConfig = (FullStackProjectConfig | SplitStackProjectConfig ) & { base: BaseConfig}
