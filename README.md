a2da260d9c62f7fed154dc54cc1fa8866451f828
Mohsen Kamrani <m.kamrani@hotmail.com>
2024-02-09 20:54:25 +1100

    add views

--- README.md
+++ README.md
@@ -1,66 +1,73 @@
-# Utopiops-kick-starter
+# Belfy
 
-<img width="20%" src="https://user-images.githubusercontent.com/15846333/164972654-03c70906-b62c-4872-affa-b39b0b8a0362.png"/>
+Belfy is a tool for generating CRUD web applications based on provided data definitions. It simplifies the process of creating full-stack or split-stack applications by automatically generating the necessary code based on the user's input.
 
-No matter how many times you do it, starting a new project from scratch means spending from a few minutes to a few hours to set up just the boilerplate.
-Utopiops-kick-starter helps you build such boilerplate for various projects in a few steps.
+## Features
 
-This repository also provides the templates for various front-end and back-end frameworks and languages with necessary components that simplify using the cloud services.
-
-Discord:
-
-https://discord.gg/KStCByYNJG
-
-![capture](https://user-images.githubusercontent.com/15846333/165880171-ba3cec08-f8ae-4bcc-a018-43e423c8cf76.gif)
+- **Automatic Code Generation:** Belfy generates code for CRUD operations based on the provided data definitions.
+- **Modular Architecture:** Easily extend Belfy with additional frameworks, languages, and features through modular plugins.
+- **Multiple Framework Support:** Currently supports React + Node.js (Express) stack, with plans to add support for more frameworks in the future.
+- **Customization:** Customize the generated code by providing specific options and configurations.
 
 
 # Status
 
 ## Currently supported
 
-### Front-end
-
-<img width="10%" src="https://user-images.githubusercontent.com/15846333/166142296-8e1c68de-601b-46de-8a9f-dd0c451129b9.png" /> <img width="10%" src="https://user-images.githubusercontent.com/15846333/166142313-828d6a84-2d76-41d5-abe7-b0595d022eac.png" /> <img width="10%" alt="Angular" src="https://user-images.githubusercontent.com/15846333/166142526-ea89df14-3af9-4e43-b9de-d391efc5df51.png" /> <img width="10%" alt="svelte" src="https://user-images.githubusercontent.com/15846333/166142628-efdca347-9ec4-42db-82a1-2ac9bd9c1ad0.png"/> 
-
-### Back-end
+### Full-stack
 
 <img width="10%" alt="node" src="https://user-images.githubusercontent.com/15846333/166142689-ddcd042c-418a-4336-ab27-76c156f3bb67.png" />
 
-### Full-stack
+### Front-end
+-
 
-TBA
+### Back-end
+-
 
-## Next release
-
-<img width="10%" alt="go" src="https://user-images.githubusercontent.com/15846333/166142724-c6bd4b2a-8204-4283-a521-33703ca8977a.png" /> 
 
 ## Future releases
 
-<img width="10%" alt="Next" src="https://user-images.githubusercontent.com/15846333/166142563-6bc6b8c9-c914-4685-8ca4-7b8cc3509f8f.png" /> <img width="10%" alt="Nuxt" src="https://user-images.githubusercontent.com/15846333/166142496-abdc74e9-2985-4c89-872f-d35e77bb64cc.png" />
 
+<img width="10%" alt="go" src="https://user-images.githubusercontent.com/15846333/166142724-c6bd4b2a-8204-4283-a521-33703ca8977a.png" /> 
+<img width="10%" src="https://user-images.githubusercontent.com/15846333/166142296-8e1c68de-601b-46de-8a9f-dd0c451129b9.png" /> <img width="10%" src="https://user-images.githubusercontent.com/15846333/166142313-828d6a84-2d76-41d5-abe7-b0595d022eac.png" /> <img width="10%" alt="Angular" src="https://user-images.githubusercontent.com/15846333/166142526-ea89df14-3af9-4e43-b9de-d391efc5df51.png" /> <img width="10%" alt="svelte" src="https://user-images.githubusercontent.com/15846333/166142628-efdca347-9ec4-42db-82a1-2ac9bd9c1ad0.png"/> 
+<img width="10%" alt="node" src="https://user-images.githubusercontent.com/15846333/166142689-ddcd042c-418a-4336-ab27-76c156f3bb67.png" />
+<img width="10%" alt="Next" src="https://user-images.githubusercontent.com/15846333/166142563-6bc6b8c9-c914-4685-8ca4-7b8cc3509f8f.png" /> <img width="10%" alt="Nuxt" src="https://user-images.githubusercontent.com/15846333/166142496-abdc74e9-2985-4c89-872f-d35e77bb64cc.png" />
 <img width="10%" src="https://user-images.githubusercontent.com/15846333/166142706-b82590f6-b73a-4eb3-96c3-956c38ca3c8d.png" /> <img width="10%" alt="php" src="https://user-images.githubusercontent.com/15846333/166142808-7cee080b-f870-4806-b5ed-665045c9c030.png" /> <img width="10%" alt="c#" src="https://user-images.githubusercontent.com/15846333/166142851-05492ac9-122c-46cd-bf96-560f49319df5.png" /> <img width="10%" alt="rust" src="https://user-images.githubusercontent.com/15846333/166142779-8f7389c2-56ce-4f8a-97c7-3420e7a6b189.png" />
 
 
 # Installation
 
+Development environment:
+`npm install`
+`npm start`
+
+
+
+Coming soon:
+
 To run the CLI using the `npm` package:
 
 ```
-npm install @utopiops/kick-starter
-ks create
+npm install belfy
+belfy generate
 ```
 
+# Documentation
+## Configuration
+Belfy uses YAML files to define data models, navigation bars, and page overrides.
 
-To run the CLI from the source-code, clone the project and in the folder you've cloned it, run the following commands in the terminal:
+entities.yaml: Defines the data models.
+navbar.yaml: Specifies the items to be displayed in the navigation bar.
+page_overrides.yaml: Allows customization of page behavior.
 
-```
-cd cli
-node bin/ks create
-```
-
-# ⭐️ `Star us`
-
-If you want to support our work - please star this project, every star makes us very happy!
-
+# Contributing
+Contributions are welcome! Please read the Contribution Guidelines (coming soon) before submitting any pull requests.
 
 
+# License
+
+This project is licensed under the Apache-2.0 License - see the LICENSE file for details.
+
+# Belfy Mascot
+
+Isn't Belfy adorable? Meet our mascot, the cute and cuddly Belfy creature!
--- src/generator/generators/fs-express-handlebars/index.ts
+++ src/generator/generators/fs-express-handlebars/index.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import { Generator, GeneratorMeta } from '../..'
 import { ProjectConfig } from '../../user-inputs/prompt-answers'
 import { writeFile, copyFolder } from '../../../utils/file'
-import { UserData } from '../../user-inputs/project-configs'
+import { Entity, UserData } from '../../user-inputs/project-configs'
 
 export class FullStackExpressHandlebarsGenerator implements Generator {
   async generate(projectConfig: ProjectConfig, userData: UserData): Promise<void> {
@@ -12,7 +12,7 @@ export class FullStackExpressHandlebarsGenerator implements Generator {
     await copyFolder(src, dest)
 
     // render the handlebars template
-    await renderTemplates(projectConfig, dest, userData)
+    await generateTemplates(dest, userData)
 
     // clean up the project
 
@@ -32,20 +32,132 @@ export class FullStackExpressHandlebarsGenerator implements Generator {
   }
 }
 
-async function renderTemplates(projectConfig: ProjectConfig, workdir: string, userData: UserData) {
-  renderModels(projectConfig, workdir, userData)
-  generateRoutes(projectConfig, workdir, userData)
+async function generateTemplates(workdir: string, userData: UserData) {
+  generateModels(workdir, userData)
+  generateRoutes(workdir, userData)
+  generateControllers(workdir, userData)
+  generateViews(workdir, userData)
 }
 
-async function generateRoutes(projectConfig: ProjectConfig, workdir: string, userData: UserData) {
+async function generateViews(workdir: string, userData: UserData) {
+  const views = userData.entities.map((entity) => generateEntityViews(workdir, entity))
+  await Promise.all(views)
+}
+
+async function generateEntityViews(workdir: string, entity: Entity) {
+  await Promise.all([
+    generateListView(workdir, entity),
+    generateDetailsView(workdir, entity),
+    generateFormView(workdir, entity),
+    generateFormView(workdir, entity, true),
+  ])
+}
+
+async function generateListView(workdir: string, entity: Entity) {
   const generatedCode = `
-import express from 'express';
+<h1>{{name}} list</h1>
+  <section>
+  <ul class="table-header">
+    ${entity.properties
+      .map((p) => {
+        return `<li>${p.name}</li>`
+      })
+      .join('\n')}
+  </ul>
+  <ul class="table-body">
+    {{#each model}}
+    <li class="table-row">
+      <ul>
+      ${entity.properties
+        .map((p) => {
+          return `<li>{{this.${p.name}}}</li>`
+        })
+        .join('\n')}
+      </ul>
+    </li>
+    {{/each}}
+  </ul>
+</section>
+`
+  await writeFile(generatedCode, path.join(workdir, 'views', 'partials', `${entity.name}-list.hbs`))
+}
+
+async function generateDetailsView(workdir: string, entity: Entity) {
+  const generatedCode = `
+<h1>${entity.name} Details</h1>
+<section>
+    <ul>
+        ${entity.properties
+          .map((p) => {
+            return `<li><strong>${p.name}:</strong> {{model.${p.name}}}</li>`
+          })
+          .join('\n')}
+    </ul>
+</section>
+`
+  await writeFile(generatedCode, path.join(workdir, 'views', 'partials', `${entity.name}-details.hbs`))
+}
+
+async function generateFormView(workdir: string, entity: Entity, isUpdate: boolean = false) {
+  const formTitle = isUpdate ? `Edit ${entity.name}` : `Add ${entity.name}`
+  const submitButtonText = isUpdate ? 'Update' : 'Submit'
+  const formAction = isUpdate
+    ? `/${entity.name.toLowerCase()}/{{model.id}}?_method=PUT`
+    : `/${entity.name.toLowerCase()}`
+
+  const generatedCode = `
+<h1>${formTitle} Form</h1>
+<form action="${formAction}" method="POST">
+    ${entity.properties
+      .map((p) => {
+        const defaultValue = isUpdate ? ` value="{{model.${p.name}}}"` : ''
+        return `
+    <div>
+        <label for="${p.name}">${p.name}:</label>
+        <input type="text" id="${p.name}" name="${p.name}" required${defaultValue}>
+    </div>`
+      })
+      .join('\n')}
+    <button type="submit">${submitButtonText}</button>
+</form>
+`
+  await writeFile(
+    generatedCode,
+    path.join(workdir, 'views', 'partials', `${entity.name}-${isUpdate ? 'edit' : 'add'}-form.hbs`),
+  )
+}
+
+async function generateControllers(workdir: string, userData: UserData) {
+  const generatedCode = `
+const { ${userData.entities.map((entity) => entity.name).join(', ')} } = require('../models');
+const generateGenericController = require('./genericController');
+
+${userData.entities
+  .map(
+    (entity) => `
+const ${entity.name.toLowerCase()}Controller = generateGenericController(${entity.name});
+
+`,
+  )
+  .join('')}
+
+module.exports = {
+${userData.entities.map((entity) => `  ${entity.name.toLowerCase()}Controller,`).join('\n')}
+};
+`
+
+  await writeFile(generatedCode, path.join(workdir, 'controllers', 'index.js'))
+}
+
+async function generateRoutes(workdir: string, userData: UserData) {
+  const generatedCode = `
+const express = require('express');
 const router = express.Router();
 
 ${userData.entities
   .map(
     (entity) => `
-import ${entity.name.toLowerCase()}Controller from '../controllers/${entity.name.toLowerCase()}Controller';
+const ${entity.name.toLowerCase()}Controller = require('../controllers/${entity.name.toLowerCase()}Controller');
 
 // Routes for ${entity.name}
 router.get('/${entity.name.toLowerCase()}', ${entity.name.toLowerCase()}Controller.getAll);
@@ -57,34 +169,16 @@ router.delete('/${entity.name.toLowerCase()}/:id', ${entity.name.toLowerCase()}C
   )
   .join('\n')}
 
-export default router;
+module.exports = router;
 `
 
-  return generatedCode
+  await writeFile(generatedCode, path.join(workdir, 'routes', 'index.js'))
 }
 
-async function renderModels(projectConfig: ProjectConfig, workdir: string, userData: UserData) {
+async function generateModels(workdir: string, userData: UserData) {
   const generatedCode = `
-import { DataTypes } from 'sequelize';
-import { sequelize } from '../config/sequelize';
-
-interface Property {
-    name: string;
-    type: string;
-    primaryKey?: boolean;
-    reference?: string;
-}
-
-interface Entity {
-    name: string;
-    properties: Property[];
-}
-
-// Define function to map types
-const mapType = (type: string): string => {
-    // Add your logic to map Sequelize data types to TypeScript types
-    return type;
-};
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/sequelize');
 
 ${userData.entities
   .map(
@@ -119,9 +213,7 @@ ${userData.entities.map((entity) => `    ${entity.name},`).join('\n')}
 };
 `
 
-  // write to file
-  console.log(generatedCode)
-  writeFile(generatedCode, path.join(workdir, 'models', 'models.js'))
+  await writeFile(generatedCode, path.join(workdir, 'models', 'index.js'))
 }
 
 function mapSequelizeType(type: string): string {
